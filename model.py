8c99cec7a2d17abdb9856ccbfa8b5febf77c5eb8
cathieG <ningmangao@gmail.com>
2025-04-09 12:29:56 -0400

    Updating model.py

--- model.py
+++ model.py
@@ -1,1 +1,30 @@
+import re
+import pandas as pd
+import torch
+from datasets import Dataset, DatasetDict
+from transformers import (
+    T5ForConditionalGeneration,
+    RobertaTokenizer,
+    TrainingArguments,
+    Trainer,
+    EarlyStoppingCallback
+)
+
+
+# Load flattened data
+def load_data():
+    df_train = pd.read_csv("flattened_train.csv")
+    df_valid = pd.read_csv("flattened_valid.csv")
+    df_test = pd.read_csv("flattened_test.csv")
+    return df_train, df_valid, df_test
+
+
+def main():
+
+    print("Start loading flattened data")
+    df_train, df_valid, df_test = load_data()
+
+
+
+
 
