4db7c1fdb9a06bcf96f7e661a88810e0bd789cd6
cathieG <161901789+cathieG@users.noreply.github.com>
2025-04-08 21:04:36 -0400

    upload requirements.py

---
+++ requirements.py
@@ -0,0 +1,10 @@
+torch==2.5.1+cu124
+torchvision==0.20.1+cu124
+torchaudio==2.5.1+cu124
+--extra-index-url https://download.pytorch.org/whl/cu124
+
+transformers
+datasets
+evaluate
+sacrebleu
+tree_sitter==0.2.0
